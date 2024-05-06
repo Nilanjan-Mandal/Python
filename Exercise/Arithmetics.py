3a1382ec1dbe49f4a8334bd38c14eb6413bf46f5
Nilanjan Mandal <nilanjan.mandal@bell.ca>
2024-05-07 00:10:51 +0530

    Modified

--- Exercise/Arithmetics.py
+++ Exercise/Arithmetics.py
@@ -1,25 +1,34 @@
+print('Lets find the sum of two numbers')
+number1 = 1.5
+number2 = 3.5
+sum = number2 + number1
+print('SUM is {0}'.format(sum))
+
+
+print('Lets find the sum of two numbers and take the input from users')
+
 a=input( " Please Enter The First  Number: " )
 b=input( " Please Enter The Second Number: " )
 addition=int(a)+int(b)
 print("The addition of two number is: ", addition)
 
+print('Lets find the subtraction of two numbers and take the input from users')
 c=input( " Please Enter The First  Number: " )
 d=input( " Please Enter The Second Number: " )
 subtract=int(d)-int(c)
 print("The subtraction of two number is: ", subtract)
 
+print('Lets find the multiplication of two numbers and take the input from users')
 e=input( " Please Enter The First  Number: " )
 f=input( " Please Enter The Second Number: " )
 multiply=int(e)*int(f)
 print("The multiplication of two number is: ", multiply)
 
-
+print('Lets find the Division of two numbers and take the input from users')
 g=input( " Please Enter The First  Number: " )
 h=input( " Please Enter The Second Number: " )
 divide=int(g)/int(h)
 print("The division of two number is: ", divide)
-
-
 
 
 
