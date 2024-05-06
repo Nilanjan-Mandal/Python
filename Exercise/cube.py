bfcef14e2bedf45ba0a989700a531c5133ed6e88
Nilanjan Mandal <nilanjan.mandal@bell.ca>
2024-05-07 00:40:24 +0530

    Modified

--- Exercise/cube.py
+++ Exercise/cube.py
@@ -1,8 +1,6 @@
 a= input("PLease Enter the Number : ")
-
 cube=int(a)*int(a)*int(a)
-
-print ( "Please find the cube of number" , cube )
+print ( "Please find the cube of {0} is {1}".format((a),(cube)))
 
 
 
--- Exercise/odd_even.py
+++ Exercise/odd_even.py
@@ -1,4 +1,8 @@
-print ("Please Enter the number you want to test odd / even")
 
-a = input()
+print("This is the program to test your number odd or even")
+number = int(input("Please enter the number you want to test odd / even : "))
 
+if number % 2 == 0:
+        print("The number {0} is Even".format(number))
+else:
+    print("The number {0} is Odd".format(number))
