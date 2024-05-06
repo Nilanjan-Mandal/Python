31a19cc76b46da0f2922d1c926a855ef37980d44
Nilanjan <nmandalstu@gmail.com>
2024-05-07 02:00:39 +0530

    Modified

---
+++ Exercise/calculator.py
@@ -0,0 +1,12 @@
+def addition(num1, num2):
+    return num1 + num2
+
+
+def subtraction(num1, num2):
+    return num1 - num2
+
+
+num1 = int(input("Please type the number: "))
+num2 = int(input("Please type the number: "))
+
+print("The Addition is {0}".format(addition(num1,num2)))
