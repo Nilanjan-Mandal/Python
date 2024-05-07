6e1149b96544bc104aec98cab2113f50bfdf02f8
Nilanjan Mandal <nmandalstu16@hotmail.com>
2024-05-07 18:19:53 +0530

    Add

--- Exercise/OOP/Class/class.py
+++ Exercise/OOP/Class/class.py
@@ -1,4 +1,16 @@
-class Person:
+class Student:
+    # default constructor
+    def __init__(self):
+        pass
+    # parametarized  constructor
     name = "Neel"
     Job = "Engineer"
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        print("Adding new person to database ...")    
+
+
+s1 = Student("Karan", 90)
+print(s1.name, s1.marks)
 
