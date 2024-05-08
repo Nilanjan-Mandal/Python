cf3108465ed12a2197d5302a8b80feb497b0cfd6
Nilanjan Mandal <nmandalstu16@hotmail.com>
2024-05-08 12:00:55 +0530

    file

---
+++ Exercise/File/file.py
@@ -0,0 +1,18 @@
+# File I/O -> (Input/Output)
+# We have to open a file before doing any kind of operations in the file. 
+
+# mode by default is 'read'. r/w
+
+# -> Let's Open the file
+f = open("demo.txt", "r") 
+
+# -> Let's Read the file
+data = f.read()
+print(data)
+print(type(data))
+
+# -> Let's Close the file
+f.close()
+
+
+
